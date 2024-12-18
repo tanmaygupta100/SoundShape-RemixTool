5eb8a75c009d6934e30a4fb3fe0bdd7ab27fffb0
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2024-12-17 23:51:22 -0800

    Setting Up React version of SoundShape
    Restarting development on SoundShape using JavaScript and React.

--- load_audio.py
+++
@@ -1,9 +0,0 @@
-from tkinter import filedialog
-import pygame
-
-def load_audio(state):
-    filepath = filedialog.askopenfilename()
-    if filepath:
-        pygame.mixer.music.load(filepath)
-        state["track"] = filepath
-        print(f"Loaded track: {filepath}")
