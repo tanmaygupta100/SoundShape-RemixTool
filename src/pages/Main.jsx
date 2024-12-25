7909f0371d007911077a48168e44e7619f72b67a
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2024-12-24 23:44:36 -0800

    SDSH-3: Mapped all main buttons
    Laid out the play/pause, forward, rewind, download, reload buttons to match the Figma design styling.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,6 +3,7 @@ import Card from '../components/card';
 import LedButton from '../components/LedButton';
 import RegButton from '../components/RegButton';
 import { ArrowDownTrayIcon} from '@heroicons/react/24/outline';
+import { ArrowPathIcon, BackwardIcon, ForwardIcon, PlayPauseIcon } from '@heroicons/react/16/solid';
 
 const Main = () => {
 
@@ -38,44 +39,104 @@ const Main = () => {
 
     return (
         <div className="flex justify-center items-center min-h-screen">
-            <Card className="flex items-center justify-center space-x-10
-                w-[900px] h-[450px]">
-                
-                <LedButton
-                    isClicked={isAClicked}
-                    onClick={handleAClick}
-                    color="orange" // Pass the color to button.js
-                    wid={100} hei={100}
-                />
-                <LedButton
-                    isClicked={isBClicked}
-                    onClick={handleBClick}
-                    color="blue"
-                    wid={100} hei={100}
-                />
-                <LedButton
-                    isClicked={isCClicked}
-                    onClick={handleCClick}
-                    color="green"
-                    wid={100} hei={100}
-                />
-                <RegButton
-                    shape=""
-                    wid={100} hei={100}
-                >
-                    <ArrowDownTrayIcon
-                        className="text-cusTxt" width={40} opacity={0.6}
-                    />
-                </RegButton>
+            <Card className="relative w-[900px] h-[500px]">
 
-                <RegButton
-                    shape='sqr'
-                    wid={100} hei={100}
-                />
-                <RegButton
-                    shape='dia'
-                    wid={100} hei={100}
-                />
+                {/* SDSH HEADER: */}
+                <h1
+                    className="heading-font text-xl opacity-80
+                        absolute left-1/2 top-3 transform -translate-x-1/2 flex flex-col items-center"
+                >
+                    SOUNDSHAPE
+                </h1>
+
+                {/* SPEED button: */}
+                <div className="absolute left-8 top-1/2 transform -translate-y-1/2 flex flex-col items-center">
+                    <p className="mr-11 mb-1 opacity-80 font-semibold">SPEED</p>
+                    <LedButton
+                        isClicked={isAClicked}
+                        onClick={handleAClick}
+                        color="orange" // Pass the color to button.js
+                        wid={100} hei={100}
+                        className='flex items-end justify-end'
+                    />
+                </div>
+
+                {/* REVERB button: */}
+                <div className="absolute right-8 top-1/2 transform -translate-y-1/2 flex flex-col items-center">
+                    <p className="mr-11 mb-1 opacity-80 font-semibold">REVERB</p>
+                    <LedButton
+                        isClicked={isBClicked}
+                        onClick={handleBClick}
+                        color="green"
+                        wid={100} hei={100}
+                    />
+                </div>
+                {/* WHITE NOISE button: */}
+                <div className="absolute left-1/2 bottom-8 transform -translate-x-1/2 flex flex-col items-center">
+                    <p className="mb-1 opacity-80 font-semibold">WHITE NOISE</p>
+                    <LedButton
+                        isClicked={isCClicked}
+                        onClick={handleCClick}
+                        color="blue"
+                        wid={100} hei={100}
+                    />
+                </div>
+
+                {/* PLAY/PAUSE button: */}
+                <div className="absolute left-1/2 top-48 transform -translate-x-1/2 flex flex-col items-center">
+                    <RegButton
+                        shape="dia"
+                        wid={60} hei={60}
+                    >
+                        <PlayPauseIcon
+                            className="text-cusTxt transform rotate-[-45deg]" width={40} opacity={0.6}
+                        />
+                    </RegButton>
+                </div>
+
+                {/* FORWARD/BACKWARD buttons: */}
+                <div className="absolute left-1/2 top-36 transform -translate-x-1/2 flex flex-row items-center gap-x-32">
+                    {/* Forward: */}
+                    <RegButton
+                        shape='cir'
+                        wid={50} hei={50}
+                    >
+                        <BackwardIcon
+                            className="text-cusTxt" width={25} opacity={0.6}
+                        />
+                    </RegButton>
+                    {/* Backward: */}
+                    <RegButton
+                        shape='cir'
+                        wid={50} hei={50}
+                    >
+                        <ForwardIcon
+                            className="text-cusTxt" width={25} opacity={0.6}
+                        />
+                    </RegButton>
+                </div>
+
+                {/* RELOAD & DOWNLOAD buttons: */}
+                <div className="absolute left-1/2 top-64 transform -translate-x-1/2 flex flex-row items-center gap-x-28">
+                    {/* Reload: */}
+                    <RegButton
+                        shape='sqr'
+                        wid={40} hei={40}
+                    >
+                        <ArrowPathIcon
+                            className="text-cusTxt" width={25} opacity={0.6}
+                        />
+                    </RegButton>
+                    {/* Download: */}
+                    <RegButton
+                        shape='sqr'
+                        wid={40} hei={40}
+                    >
+                        <ArrowDownTrayIcon
+                            className="text-cusTxt" width={25} opacity={0.7}
+                        />
+                    </RegButton>
+                </div>
             </Card>
         </div>
     );
